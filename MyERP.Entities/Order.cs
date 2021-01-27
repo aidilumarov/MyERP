0b9a4528583ebfd3511d9dd0b311c1e4c2344412
Aidil Umarov <aidildev@tuta.io>
2021-01-27 11:58:04 +0600

    Created a controller and entity for order

---
+++ MyERP.Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyERP.Entities
+{
+    public class Order : BaseEntity
+    {
+        public decimal Price { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
