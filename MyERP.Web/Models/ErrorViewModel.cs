4e11d672576eb729743194c218f0fc9da981cf94
Aidil Umarov <aidildev@tuta.io>
2021-01-27 11:49:15 +0600

    Add project files.

---
+++ MyERP.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyERP.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
